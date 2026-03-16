4861d5a88250419b991ac7599b74be6e3dae74df
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-16 16:11:48 +0600

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,3 +16,48 @@ else if  (month == 3)
 else 
     monthName = "Unknown";
 
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    case 3:
+        monthName = "March";
+        break;
+    default:
+        monthName = "Unknown";
+        break;
+}
+
+Console.WriteLine($" Then month is {monthName}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
