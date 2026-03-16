c2f819e37039a5e75a3181e472216a6c0b7efe56
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-16 14:44:06 +0600

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int month = 5;
+string monthName;
+
+if (month == 1)
+{
+    monthName = "January";
+}
+
+else if  (month == 2)
+    monthName = "February";
+else if  (month == 3)
+    monthName = "March";
+else 
+    monthName = "Unknown";
+
