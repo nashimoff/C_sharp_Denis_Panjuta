b98b9940842d79f47cd2175c78948ad24a4d0c7e
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-17 11:00:58 +0000

    a

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string question1 = "What is the capital of Germany";
+string answer1 = "Berlin";
