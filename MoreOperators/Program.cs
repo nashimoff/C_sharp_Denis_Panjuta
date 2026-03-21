9903c087f84744f79d42f168b048156d843a6ff6
nashimoff <nurlan.ashimoff@gmail.com>
2026-03-21 16:25:51 +0600

    a

---
+++ MoreOperators/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int num1 = 10;
+int num2 = 3;
+
+// Modulo Operator
+int result = num1 % num2;
+// 10 / 3 = remiander 1
+
+Console.WriteLine(result);
+
+// incrementing of int
+
+// Console.WriteLine($"Num is {num}");
+//
+// // pre-incrementing
+// Console.WriteLine("Num is {0}", ++num);
+// Console.WriteLine("Num is {0}", num++);
+// Console.WriteLine("Num is {0}", num);
+//
+// // decrementing
+// num--;
+// Console.WriteLine($"Num is {num}");
+// Console.WriteLine("Num is {0}", --num);
+// Console.WriteLine("Num is {0}", num--);
+// Console.WriteLine("Num is {0}", num);
+/*
+num += 30;
+Console.WriteLine("Num is {0}", num);
+num -= 10;
+Console.WriteLine("Num is {0}", num);
+num *= 10;
+Console.WriteLine("Num is {0}", num);
+num /= 20;
+Console.WriteLine("Num is {0}", num);
+*/
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// creating an instance of the Random Class
+Random random = new Random();
+// this will give us a random number between 1 and 10
+int randomNumber = random.Next(1, 11);
+
+Console.WriteLine("Guess the number\n");
+
+string inputString = Console.ReadLine();
+int num1 = 0;
+
+bool isNumber = int.TryParse(inputString, out num1);
+
+if (isNumber)
+{
+    if (num1 == randomNumber)
+    {
+        Console.WriteLine("You guessed right!\n");
+    }
+    else
+    {
+        Console.WriteLine("You guessed wrong, try again\n");
+    }
+}
+else
+{
+    Console.WriteLine("Haha you troll. You should've entered a number\n");
+}
+Console.ReadKey();
+
+
+
+
